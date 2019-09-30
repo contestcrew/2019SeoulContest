14a0efec44400f8a50a3c2cf377e1df199a20399
furydeveloper <furysecu@gmail.com>
2019-09-30 16:12:03 +0900

    BEFORE: FCM

--- FirstCitizen/FirstCitizen/AppDelegate.swift
+++ FirstCitizen/FirstCitizen/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,8 +15,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
   let vc = SplashViewController()
   
-  
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    FirebaseApp.configure()
+    
     runApp()
     return true
   }
